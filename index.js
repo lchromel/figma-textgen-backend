31863cf47fa6679c51702494bc75cea9bf65a0fa
lchromel <dm.vershinin@gmail.com>
2025-05-19 22:13:32 +0400

    fix bags 2

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ import cors from 'cors';
 
 dotenv.config();
 const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.use(cors());
 app.use(express.json());
 
